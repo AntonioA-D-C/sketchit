d9eec0f8b372e23a0a1107da4a92379b5a100e9a
Antonio-A-D-C <anton.y01@hotmail.com>
2024-05-14 17:59:17 -0400

    Save current data

--- app/Models/post.php
+++ app/Models/post.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Spatie\Tags\HasTags;
 
 class post extends Model
 {
+    use HasTags;
     use HasFactory;
     protected $guarded =[];
     public function user(){
